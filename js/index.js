55112fae9db4df68543b71a0dde0832a3f4a60aa
Your Name <you@example.com>
2024-01-15 12:33:49 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenavs").style.width = "320px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenavs").style.width = "0";
+}
+
+
